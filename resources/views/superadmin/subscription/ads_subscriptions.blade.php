67731270e86c3115342a3e2bbb84bf8458ed28de
Abid Ali <80045928+abidali05@users.noreply.github.com>
2025-08-08 11:05:50 +0500

    subscription ads

--- resources/views/superadmin/subscription/ads_subscriptions.blade.php
+++ resources/views/superadmin/subscription/ads_subscriptions.blade.php
@@ -83,7 +83,8 @@
         .nav-link:active {
             color: #ffffff !important;
         }
-              .table>:not(caption)>*>* {
+
+        .table>:not(caption)>*>* {
             padding: 0rem .5rem;
             color: var(--bs-table-color-state, var(--bs-table-color-type, var(--bs-table-color)));
             background-color: var(--bs-table-bg);
@@ -96,12 +97,24 @@
             padding: 0px 10px 5px 10px;
             border-bottom: 1px solid rgba(0, 0, 0, 0.3);
         }
-        div.dt-container .dt-length, div.dt-container .dt-search, div.dt-container .dt-info, div.dt-container .dt-processing, div.dt-container .dt-paging {
-    color: inherit;
-    display: flex
-;
-    justify-content: end;
-}
+
+        div.dt-container .dt-length,
+        div.dt-container .dt-search,
+        div.dt-container .dt-info,
+        div.dt-container .dt-processing,
+        div.dt-container .dt-paging {
+            color: inherit;
+            display: flex;
+            justify-content: end;
+        }
+
+        .bike-column-search {
+            width: 90px;
+            font-size: 10px;
+            border: 1px solid #D9D9D9;
+            border-radius: 2px;
+            padding: 2px;
+        }
     </style>
     <!-- Buttons to toggle between Ads and Services -->
     <div class="container my-3">
@@ -274,7 +287,7 @@
                     <div class="tab-pane fade show active" id="tab1" role="tabpanel">
                         <div class="table-responsive ">
 
-                           @if (count($userSubscriptions))
+                            @if (count($userSubscriptions))
                                 <div class="card mb-4">
                                     <div class="card-body p-0">
                                         <table class="table table-bordered mb-0 subscription-datatable">
@@ -345,20 +358,46 @@
                     lengthChange: false,
                     searching: true,
                     ordering: true,
-                    scrollX: true,
+                    scrollX: false,
                     order: [
                         [0, 'asc']
                     ],
                     language: {
                         search: "Search: "
                     },
-                                dom: `
+                    dom: `
   <"search-wrapper mb-3"f>
   <"pagination-wrapper d-flex justify-content-between align-items-center mb-3"i p>
   rt
   <"pagination-wrapper d-flex justify-content-between align-items-center mt-3"i p>
   <"clear">
 `
+                });
+
+                // Add search row
+
+                $(this).find('thead').append('<tr class="search-row"></tr>');
+
+                $(this).find('thead th').each(function(index) {
+                    var title = $(this).text().trim();
+                    var searchHtml = '';
+
+                    if (['User Name', 'Start Date', 'Email', 'Subscription ID', 'Plan',
+                            'Start Date', 'End Date', 'Status'
+                        ]
+                        .includes(title)) {
+                        searchHtml = '<input type="text" placeholder="Search ' + title +
+                            '" class="bike-column-search"/>';
+                    }
+
+                    $(this).closest('thead').find('.search-row').append('<th>' + searchHtml +
+                        '</th>');
+                });
+
+                // Apply search functionality
+                $(this).find('.search-row input, .search-row select').on('keyup change', function() {
+                    var columnIndex = $(this).closest('th').index();
+                    table.column(columnIndex).search(this.value).draw();
                 });
             });
         });
