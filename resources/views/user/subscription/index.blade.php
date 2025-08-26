8d958c2445c6ba9e918794b9d22006c84a6e6019
anabkhan00 <anabkhanm@gmail.com>
2025-08-26 11:24:35 +0500

    subscription

--- resources/views/user/subscription/index.blade.php
+++ resources/views/user/subscription/index.blade.php
@@ -504,9 +504,9 @@
                                                         class="twenty mt-5 d-flex justify-content-between align-items-center {{ $ads_plan->metadata->is_recomended == '1' ? 'textclas' : '' }}">
                                                         {{ $ads_plan->name }}
                                                         @if ($loop->first)
-                                                            <img src="{{ asset('web/images/Group (2).svg') }}"
-                                                                style="height:80px;width:80px" class="img-fluid md-3"
-                                                                alt="...">
+                                                           <img src="{{ asset('web/images/free_trail.svg') }}"
+                                                            style="height:60px;width:60px" class="img-fluid mb-3"
+                                                            alt="Free Trial">
                                                         @else
                                                             <img src="{{ asset('web/images/other_icon.svg') }}"
                                                                 style="height:60px;width:10px" class="img-fluid mb-3">
@@ -603,9 +603,9 @@
                                                     <p
                                                         class="twenty mt-5 d-flex justify-content-between align-items-center {{ $plan->metadata->is_recomended == '1' ? 'textclas' : '' }}">
                                                         {{ $plan->name }} @if ($loop->first)
-                                                            <img src="{{ asset('web/images/Group (2).svg') }}"
-                                                                style="height:80px;width:80px" class="img-fluid md-3"
-                                                                alt="...">
+                                                        <img src="{{ asset('web/images/free_trail.svg') }}"
+                                                            style="height:60px;width:60px" class="img-fluid mb-3"
+                                                            alt="Free Trial">
                                                         @else
                                                             <img src="{{ asset('web/images/other_icon.svg') }}"
                                                                 style="height:60px;width:10px" class="img-fluid mb-3">
